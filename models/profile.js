b1b216d3832a44c913ead3926b37f0568147b5d3
ralrubayee <ralrubayee@gmail.com>
2021-12-16 16:00:51 -0500

    profile model

--- models/profile.js
+++ models/profile.js
@@ -1,8 +1,24 @@
 import mongoose from 'mongoose'
 
 const profileSchema = new mongoose.Schema({
-  email: {type: String, required: true, lowercase: true, unique: true},
   name: String,
+  avatar: {
+    type: String,
+    required: true
+  },
+  github: {
+    type: String,
+    default: null
+  },
+  linkedin: {
+    type: String,
+    default: null
+  },
+  bio: {
+    type: String,
+    default: null
+  },
+  
 },{
     timestamps: true,
 })
