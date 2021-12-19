a9db639f01ea113aebb65e2faff5a4d76efa4053
e.lascano <emilas5678@gmail.com>
2021-12-19 10:39:30 -0500

    complete hiding button

--- src/pages/Profiles/ProfileCard.jsx
+++ src/pages/Profiles/ProfileCard.jsx
@@ -3,14 +3,18 @@ import ProfileAction from './ProfileAction'
 
 const ProfileCard = (props) => {
   console.log("profilecard props", props.friends)
-  
+
+const isFriend = props.myFriends.includes(props.profileId)
+    
+
 
   return(
     <div className="card-container">
       <div className="card">
         <img className="card-content" src={props.img} alt={props.name}  />
         <p className="card-title">{props.name}</p>
-        <ProfileAction onClick={props.onClick} />
+        {isFriend ? "" :
+        <ProfileAction onClick={props.onClick}/>  }
       </div>
     </div>
   )
