f0fe214fa4dbbe2ba3a54ce3718f073d1473466e
emma <emma.ronaldo7@gmail.com>
2021-12-18 21:27:00 -0500

    api working

---
+++ src/services/jobs.js
@@ -0,0 +1,12 @@
+// import * as tokenService from './tokenService'
+const BASE_URL = '/api/jobs/';
+
+export const findJob = async (category) => {
+	try {
+		const res = await fetch(`${BASE_URL}?category=${category}`);
+		const data = await res.json();
+		return data;
+	} catch (error) {
+		throw error;
+	}
+};
