d28dc228f10a946656a054492b8ec5d2a91df970
Ali A <alsamawiali4@gmail.com>
2021-12-20 16:56:38 -0500

    profileInfi header

--- src/pages/ProfileInfo/ProfileHeader.jsx
+++ src/pages/ProfileInfo/ProfileHeader.jsx
@@ -8,8 +8,12 @@ console.log('ProfileHeader', props)
   return(
   <>
   {/* <p>{props.user.name}</p> */}
-  <h2>{props.profile.name}</h2>
-  
+  <p>Bio: {props.profile.bio}</p>
+  <h1 >gitHub: {props.profile.github}</h1>
+  <h1> Linkedin: {props.profile.linkedin}</h1>
+  <h1>Name: {props.profile.name}</h1>
+  <img src={props.profile.avatar} />
+
   </>
   )
 }
