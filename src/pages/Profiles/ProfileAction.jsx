f0162d0d544b28fe77a68c458a3ef622bc0acb14
e.lascano <emilas5678@gmail.com>
2021-12-20 16:26:09 -0500

    use fontAwesome

--- src/pages/Profiles/ProfileAction.jsx
+++ src/pages/Profiles/ProfileAction.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProfileAction = (props) => {
   return(
     <>
-      <button onClick={props.onClick}>+</button>
+      <button className='profile-button' onClick={props.onClick}><i class="fas fa-user-plus"></i></button>
     </>
   )
 }
