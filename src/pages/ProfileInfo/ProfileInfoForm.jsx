c18e17090b11c1323a111263f32c6d64ce927199
Ali A <alsamawiali4@gmail.com>
2021-12-20 18:19:44 -0500

    handle submit

--- src/pages/ProfileInfo/ProfileInfoForm.jsx
+++ src/pages/ProfileInfo/ProfileInfoForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router'
+import * as profileService from "../../services/profileService"
 
 
 
@@ -9,8 +10,22 @@ const ProfileInfoForm=() =>{
     linkedin:"",
     github:"",
   })
-  const navigate = useNavigate()
+  const handleChange = evt =>{
+    setFormData({...formData, [evt.target.name]: [evt.target.value]})
+  }
+  // useEffect (()=>{
+  //   const profileData = async await profileService.updateProfile()=>
+  // })
 
+const handleSumbit = async (e) =>{
+  e.preventDefault()
+  try {
+    const editProfile = await profileService.updateProfile(formData)
+    
+  } catch (error) {
+    throw error
+  }
+}
 
   // console.log(props)
 
