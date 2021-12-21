bb9f9250d2252f0147115cdf8dbb46f5ef94956f
Ali A <alsamawiali4@gmail.com>
2021-12-21 18:07:44 -0500

    stly 3

--- src/pages/ProfileInfo/ProfileHeader.jsx
+++ src/pages/ProfileInfo/ProfileHeader.jsx
@@ -9,10 +9,12 @@ console.log('ProfileHeader', props)
 
   return(
   <>
+  <div className= "Bio">
   <h1>{props.profile.name} <img src={props.profile.avatar} alt={props.profile.name} /></h1>
   <h1>GitHub: {props.profile.github}</h1>
   <h1>Bio: {props.profile.bio}</h1>
   <h1>Linkedin: {props.profile.linkedin}</h1>
+  </div>
   </>
   
   )
