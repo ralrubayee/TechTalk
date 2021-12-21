1f12bfb94a800cf81f75e445f62c952a983c508c
e.lascano <emilas5678@gmail.com>
2021-12-20 19:25:36 -0500

    navbar in progress

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,19 +1,45 @@
 import { Link } from 'react-router-dom'
+import logo from '../../assets/images/conversation.png'
 
 const NavBar = ({ user, handleLogout }) => {
+  console.log(user)
   return (
     <>
       {user ?
-        <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="/posts">Post</Link></li>
-            <li><Link to="/jobs">Jobs</Link></li>
-            <li><Link to="/profiles/:id">ProfileInfo</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+        // <nav>
+        //   <ul>
+        //     <li>Welcome, {user.name}</li>
+        //     <li><Link to="/profiles">Profiles</Link></li>
+        //     <li><Link to="/posts">Post</Link></li>
+        //     <li><Link to="/jobs">Jobs</Link></li>
+        //     <li><Link to="/profiles/:id">ProfileInfo</Link></li>
+        //     <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+        //   </ul>
+        // </nav>
+
+
+        <nav className="navbar">
+          <div class="logo">
+              TechTalk<img 
+                src={logo} 
+                alt="TechTalk Logo"
+                id="logo-img"
+              /> 
+          </div>
+
+          <ul class="nav-links">
+            <input type="checkbox" id="checkbox_toggle" />
+            <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+
+            <div class="menu">
+              
+            </div>
+
           </ul>
+
         </nav>
+          
+
       :
         <nav>
           <ul>
@@ -21,6 +47,9 @@ const NavBar = ({ user, handleLogout }) => {
             <li><Link to="/signup">Sign Up</Link></li>
           </ul>
         </nav>
+
+
+
       }
     </>
   )
