0293f17915e7f84c3dce82795f018b3c60af4a51
e.lascano <emilas5678@gmail.com>
2021-12-21 10:55:18 -0500

    create responsive navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,54 +5,39 @@ const NavBar = ({ user, handleLogout }) => {
   console.log(user)
   return (
     <>
-      {user ?
-        // <nav>
-        //   <ul>
-        //     <li>Welcome, {user.name}</li>
-        //     <li><Link to="/profiles">Profiles</Link></li>
-        //     <li><Link to="/posts">Post</Link></li>
-        //     <li><Link to="/jobs">Jobs</Link></li>
-        //     <li><Link to="/profiles/:id">ProfileInfo</Link></li>
-        //     <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-        //   </ul>
-        // </nav>
-
-
-        <nav className="navbar">
-          <div class="logo">
-              TechTalk<img 
-                src={logo} 
-                alt="TechTalk Logo"
-                id="logo-img"
+    <header>
+      <nav className="navbar">
+        <div class="logo">
+            TechTalk<img 
+              src={logo} 
+              alt="TechTalk Logo"
+              id="logo-img"
               /> 
-          </div>
-
-          <ul class="nav-links">
-            <input type="checkbox" id="checkbox_toggle" />
-            <label for="checkbox_toggle" class="hamburger">&#9776;</label>
-
-            <div class="menu">
-              
-            </div>
-
-          </ul>
-
-        </nav>
+        </div>
+        <ul class="nav-links">
+          <input type="checkbox" id="checkbox_toggle" />
+          <label for="checkbox_toggle" class="hamburger">&#9776;</label>
           
-
-      :
-        <nav>
-          <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-          </ul>
-        </nav>
-
-
-
-      }
+          <div class="menu">
+            {user ?
+            <>
+              <li><Link to="/profiles">Profiles</Link></li>
+              <li><Link to="/posts">Post</Link></li>
+              <li><Link to="/jobs">Jobs</Link></li>
+              <li><Link to="/profiles/:id">ProfileInfo</Link></li>
+              <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+            </>
+              :
+            <>
+              <li><Link to="/login">Log In</Link></li>
+              <li><Link to="/signup">Sign Up</Link></li>
+            </>
+            }
+          </div>      
+        </ul>
+      </nav>
+</header>
     </>
   )
 }
-
 export default NavBar
