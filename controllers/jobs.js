8272671cd3b58f1017fbcfbd159966ee31acdfe2
e.lascano <emilas5678@gmail.com>
2021-12-22 18:26:10 -0500

    job section styling

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -2,12 +2,10 @@ import axios from 'axios';
 const key = process.env.API_KEY;
 
 const page = 1;
-// const category = 'Software Engineer'
 
 async function index(req, res) {
 	try {
 		const category = req.query.category;
-		console.log(req.query);
 		const BASE_URL = `https://www.themuse.com/api/public/jobs?category=${category}&page=${page}&descending=true?api_key=${key}`;
 		const response = await axios.get(BASE_URL);
 		res.status(200).json(response.data);
