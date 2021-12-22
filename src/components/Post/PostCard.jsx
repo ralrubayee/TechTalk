6f7c47713223d1838611ff03e6cf45c89c25ac34
emma <emma.ronaldo7@gmail.com>
2021-12-22 13:45:41 -0500

    edit button

--- src/components/Post/PostCard.jsx
+++ src/components/Post/PostCard.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React,{useState}from 'react'
 import PostActions from './PostActions';
 import EditPost from './EditPost';
 
 
 
 const PostCard = (props) => {
-  console.log('something',props.post.added_by._id)
+  const [boolean, setBoolean]= useState(false)
+  const handleClick = ()=> {
+          boolean?
+          setBoolean(false)
+          :
+          setBoolean(true)
+          return
+        }
   return (
     <div className="post-card">
       <div className="card-header">
@@ -17,14 +24,22 @@ const PostCard = (props) => {
         <p>{props.post.text}</p>
       </div>
       <div className="comment-dropdown"></div>
-     
-     {props.user.profile === props.post.added_by._id ?
+      {props.user.profile === props.post.added_by._id ?
+      
+      <>
+      <button onClick={()=> handleClick()}>edit</button>
+    
+      {boolean ? 
       <EditPost post={props.post} 
       user={props.user}
       handleUpdatePost={props.handleUpdatePost}
       />
+      
       :''
-     }
+}
+     </>
+      :''
+      }
     </div>
   )
 }
